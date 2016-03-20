0290204f9a669760f73655932dd19ab366e04411
Azizur Rahman <Sarzil@azizurs-macbook-pro.local>
2016-03-19 21:30:07 -0400

    Initial Commit

---
+++ InstagramDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InstagramDemo
+//
+//  Created by Azizur Rahman on 3/19/16.
+//  Copyright © 2016 Azizur Rahman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
